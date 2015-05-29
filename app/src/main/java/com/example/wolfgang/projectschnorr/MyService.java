13dd6e79d61663891f95207d8668f86eb0a34024
Pascal <pascal.baumgartner@a1.net>
2015-05-29 12:51:14 +0200

    mit dem erstellen des Service begonnen Pascal Baumgartner

---
+++ app/src/main/java/com/example/wolfgang/projectschnorr/MyService.java
@@ -0,0 +1,35 @@
+package com.example.wolfgang.projectschnorr;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by Pascal on 29.05.2015.
+ */
+public class MyService extends Service
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        return null;
+    }
+}
